a79f4e55d7710d8f8e9cbec4a3b9229ab2ec0080
billyggroves <billyggroves@gmail.com>
2025-06-27 00:12:51 -0400

    add static data and set main to delete generated data and copy from static

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
 from textnode import *
+from os import path, listdir, mkdir
+from shutil import rmtree, copytree
+import time
+
+path_to_static = "./static/"
+path_to_public = "./public/"
+
+def delete_public():
+    if path.exists(path_to_public) and len(listdir(path_to_public)) > 0:
+        print(listdir(path_to_public))
+        rmtree(path_to_public)
+    return
+
+def copy_files():
+    if path.exists(path_to_static) and len(listdir(path_to_static)) > 0:
+        print(listdir(path_to_static))
+        copytree(path_to_static,path_to_public)
+    return
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    # print(node)
-
-
-        
+    delete_public()
+    copy_files()
 
 main()
